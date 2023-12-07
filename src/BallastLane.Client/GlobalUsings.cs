2f51929c46099f3ba149a1fd652ace6f67eda9f5
Marco Medrano <marco.medrano.garay@gmail.com>
2023-12-07 02:35:22 -0400

    feat: Metamask auth working

--- src/BallastLane.Client/GlobalUsings.cs
+++ src/BallastLane.Client/GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using BallastLane.Client.Extensions;
 global using BallestLane.Dtos.User;
+global using BallastLane.Client.Infraestructure;
 
--- src/BallastLane.Client/Program.cs
+++ src/BallastLane.Client/Program.cs
@@ -4,12 +4,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 var services = builder.Services;
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 services.AddMudServices();
 services.AddAuthServices();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 await builder.Build().RunAsync();
